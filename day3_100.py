c1eb2edb834badf0538fcb6e860da2bf37a101b3
Electrigeneer <137040325+electrigeneer@users.noreply.github.com>
2023-12-13 13:11:01 +0300

    day3_100.py | 13.12.2023

---
+++ day3_100.py
@@ -0,0 +1,8 @@
+nameFood = input("Name a food > ")
+nameType = input("Name a type of food > ")
+nameMethod = input("Name a method of cooking > ")
+burnedFood = input("What word describes burned food? >")
+nameDiyItem = input("Name a DIY item > ")
+
+print("MENU")
+print(nameMethod, nameFood, "with", burnedFood, nameType,"on a bed of", nameDiyItem)
